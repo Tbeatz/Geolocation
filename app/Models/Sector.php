4345ad7f2cb3d2824dfffd79a40a6aedc903165d
Thu Ta <61737224+Tbeatz@users.noreply.github.com>
2023-05-15 19:34:10 +0630

    sector column add seeder add

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Sector extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'name_mm'];
+    protected $fillable = ['name', 'name_mm', 'icon'];
 }
--- database/seeders/SectorSeeder.php
+++ database/seeders/SectorSeeder.php
@@ -18,8 +18,63 @@ class SectorSeeder extends Seeder
         Sector::truncate();
         Schema::enableForeignKeyConstraints();
         Sector::create([
-            'name' => 'Industry',
-            'name_mm' => 'စက်မှု',
+            'name' => 'Agriculture',
+            'name_mm' => 'စိုက်ပျိုးရေး',
+        ]);
+
+        Sector::create([
+            'name' => 'Livestock & Fisheries',
+            'name_mm' => 'မွေးမြူရေးနှင့်ရေလုပ်ငန်း',
+        ]);
+
+        Sector::create([
+            'name' => 'Mining',
+            'name_mm' => 'သတ္တုတွင်း',
+        ]);
+
+        Sector::create([
+            'name' => 'Manufacturing',
+            'name_mm' => 'ကုန်ထုတ်လုပ်မှု',
+        ]);
+
+        Sector::create([
+            'name' => 'Power',
+            'name_mm' => 'စွမ်းအား',
+        ]);
+
+        Sector::create([
+            'name' => 'Oil and Gas',
+            'name_mm' => 'ရေနံနှင့်သဘာဝဓာတ်ငွေ့',
+        ]);
+
+        Sector::create([
+            'name' => 'Construction',
+            'name_mm' => 'ဆောက်လုပ်ရေး',
+        ]);
+
+        Sector::create([
+            'name' => 'Transport & Communication',
+            'name_mm' => 'ပို့ဆောင်ရေးနှင့်ဆက်သွယ်ရေး',
+        ]);
+
+        Sector::create([
+            'name' => 'Hotel & Tourism',
+            'name_mm' => 'ဟိုတယ်နှင့်ခရီးသွားလုပ်ငန်း',
+        ]);
+
+        Sector::create([
+            'name' => 'Real Estate',
+            'name_mm' => 'အိမ်ခြံမြေအကျိုးဆောင်',
+        ]);
+
+        Sector::create([
+            'name' => 'Industrial Estate',
+            'name_mm' => 'စက်မှုဇုန်',
+        ]);
+
+        Sector::create([
+            'name' => 'Other Services',
+            'name_mm' => 'အခြားဝန်ဆောင်မှုများ',
         ]);
     }
 }
