5f86cd69d4ced5c9d0619e97438eddb756dd85f6
Thu Ta <61737224+Tbeatz@users.noreply.github.com>
2023-05-22 21:05:07 +0630

    sector icons seed

--- database/seeders/SectorSeeder.php
+++ database/seeders/SectorSeeder.php
@@ -20,61 +20,73 @@ class SectorSeeder extends Seeder
         Sector::create([
             'name' => 'Agriculture',
             'name_mm' => 'စိုက်ပျိုးရေး',
+            'icon' => 'img/icons/agriculture.png',
         ]);
 
         Sector::create([
             'name' => 'Livestock & Fisheries',
             'name_mm' => 'မွေးမြူရေးနှင့်ရေလုပ်ငန်း',
+            'icon' => 'img/icons/livestock.png',
         ]);
 
         Sector::create([
             'name' => 'Mining',
             'name_mm' => 'သတ္တုတွင်း',
+            'icon' => 'img/icons/mining.png',
         ]);
 
         Sector::create([
             'name' => 'Manufacturing',
             'name_mm' => 'ကုန်ထုတ်လုပ်မှု',
+            'icon' => 'img/icons/manufacturing.png',
         ]);
 
         Sector::create([
             'name' => 'Power',
             'name_mm' => 'စွမ်းအား',
+            'icon' => 'img/icons/power.png',
         ]);
 
         Sector::create([
             'name' => 'Oil and Gas',
             'name_mm' => 'ရေနံနှင့်သဘာဝဓာတ်ငွေ့',
+            'icon' => 'img/icons/oil-gas.png',
         ]);
 
         Sector::create([
             'name' => 'Construction',
             'name_mm' => 'ဆောက်လုပ်ရေး',
+            'icon' => 'img/icons/construction.png',
         ]);
 
         Sector::create([
             'name' => 'Transport & Communication',
             'name_mm' => 'ပို့ဆောင်ရေးနှင့်ဆက်သွယ်ရေး',
+            'icon' => 'img/icons/transport-communication.png',
         ]);
 
         Sector::create([
             'name' => 'Hotel & Tourism',
             'name_mm' => 'ဟိုတယ်နှင့်ခရီးသွားလုပ်ငန်း',
+            'icon' => 'img/icons/hotel-tourism.png'
         ]);
 
         Sector::create([
             'name' => 'Real Estate',
             'name_mm' => 'အိမ်ခြံမြေအကျိုးဆောင်',
+            'icon' => 'img/icons/real-estate.png'
         ]);
 
         Sector::create([
             'name' => 'Industrial Estate',
             'name_mm' => 'စက်မှုဇုန်',
+            'icon' => 'img/icons/industrial-estate.png'
         ]);
 
         Sector::create([
             'name' => 'Other Services',
             'name_mm' => 'အခြားဝန်ဆောင်မှုများ',
+            'icon' => 'img/icons/other-service.png'
         ]);
     }
 }
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -54,7 +54,7 @@
                 </div>
             </div>
             <div class="leaflet-control reset">
-                <div class="sizeSetContainer vertical-center">
+                <div class="sizeSetContainer vertical-center fixed">
                     <button type="button" id="ScreenSize" class="mb-1 bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-3 mx-2 rounded">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
